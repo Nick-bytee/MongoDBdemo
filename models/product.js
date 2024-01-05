33d5bd8f51bb3505c17d082cdb214c9c53b3c8cd
nickbytee <nikhilkaushik2228@gmail.com>
2024-01-05 14:24:00 +0530

    added admin and shop functions

--- models/product.js
+++ models/product.js
@@ -1,20 +1,71 @@
+const mongodb = require("mongodb");
 const getdb = require("../util/database").getdb;
 
 class Product {
-  constructor(title, price, description, imageUrl) {
+  constructor(title, price, description, imageUrl, id) {
     (this.title = title),
       (this.price = price),
       (this.description = description),
-      (this.imageUrl = imageUrl);
+      (this.imageUrl = imageUrl),
+      (this._id = id ? new mongodb.ObjectId(id) : null);
   }
 
   save() {
     const db = getdb();
-    return db
-      .collection("products")
-      .insertOne(this)
+    let dbOp;
+    if (this._id) {
+      dbOp = db
+        .collection("products")
+        .updateOne({ _id: this._id }, { $set: this });
+    } else {
+      console.log("working");
+      dbOp = db.collection("products").insertOne(this);
+    }
+    return dbOp
       .then((result) => console.log(result))
       .catch((err) => console.log(err));
+  }
+
+  static fetchAll() {
+    const db = getdb();
+    return db
+      .collection("products")
+      .find()
+      .toArray()
+      .then((products) => {
+        // console.log(products);
+        return products;
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  static findById(prodId) {
+    const db = getdb();
+    return db
+      .collection("products")
+      .findOne({ _id: new mongodb.ObjectId(prodId) })
+      .then((product) => {
+        // console.log(product);
+        return product;
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  static deleteById(prodId) {
+    const db = getdb();
+    return db
+      .collection("products")
+      .deleteOne({ _id: new mongodb.ObjectId(prodId) })
+      .then((product) => {
+        return product;
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   }
 }
 
