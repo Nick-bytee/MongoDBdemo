945bd2904915bdabc5b49cf88c25f6b7147102c3
nickbytee <nikhilkaushik2228@gmail.com>
2024-01-05 12:32:03 +0530

    configured mongoDB

--- util/database.js
+++ util/database.js
@@ -1,8 +1,30 @@
-const Sequelize = require('sequelize');
+const mongodb = require("mongodb");
+const MongoClient = mongodb.MongoClient;
 
-const sequelize = new Sequelize('node-complete', 'root', 'nodecomplete', {
-  dialect: 'mysql',
-  host: 'localhost'
-});
+let db;
 
-module.exports = sequelize;
+const uri =
+  "mongodb+srv://nick2228:Nick4321@cluster0.q3yaxzn.mongodb.net/?retryWrites=true&w=majority";
+
+const mongoConnect = (callback) => {
+  MongoClient.connect(uri)
+    .then((client) => {
+      console.log("connected to database");
+      db = client.db();
+      callback();
+    })
+    .catch((err) => {
+      console.log(err);
+      throw err;
+    });
+};
+
+const getdb = () => {
+  if (db) {
+    return db;
+  }
+  throw "No Database Found";
+};
+
+exports.mongoConnect = mongoConnect;
+exports.getdb = getdb;
