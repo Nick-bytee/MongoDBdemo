7bcc36e53c367c5b078609ba3be7cfff8d8b1d11
nickbytee <nikhilkaushik2228@gmail.com>
2024-01-06 10:49:04 +0530

    fixed cart

--- app.js
+++ app.js
@@ -19,9 +19,10 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use((req, res, next) => {
+  console.log();
   User.findById("6597c5d86b6135c3c34114f7")
     .then((user) => {
-      req.user = user;
+      req.user = new User(user.name, user.email, user.cart, user._id);
       next();
     })
     .catch((err) => console.log(err));
--- models/user.js
+++ models/user.js
@@ -2,13 +2,32 @@ const mongodb = require("mongodb");
 const getdb = require("../util/database").getdb;
 
 class User {
-  constructor(name, email) {
-    (this.name = name), (this.email = email);
+  constructor(name, email, cart, id) {
+    this.name = name;
+    this.email = email;
+    this.cart = cart;
+    this._id = id;
   }
 
   add() {
     const db = getdb();
     return db.collection("users").inserOne(this);
+  }
+
+  addToCart(product) {
+    // const cartProduct = this.cart.items.findIndex((cp) => {
+    //   return cp._id === product._id;
+    // });
+    const updatedCart = {
+      items: [{ productId: new mongodb.ObjectId(product._id), quantity: 1 }],
+    };
+    const db = getdb();
+    return db
+      .collection("users")
+      .updateOne(
+        { _id: new mongodb.ObjectId(this._id) },
+        { $set: { cart: updatedCart } }
+      );
   }
 
   static findById(userId) {
